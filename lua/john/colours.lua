2811f3831c51c70270836e0047ea6f47927b3f6c
John Fulton <john@john-fulton.com>
2024-04-16 20:11:34 +0800

    set colour options to apply after plugins have loaded

--- lua/john/colours.lua
+++ lua/john/colours.lua
@@ -1,4 +1,8 @@
-color = color or "carbonfox"
-vim.cmd.colorscheme(color)
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.cmd([[
+  augroup MyColorScheme
+    autocmd!
+    autocmd VimEnter * ++once colorscheme carbonfox
+    autocmd VimEnter * ++once lua vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
+    autocmd VimEnter * ++once lua vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
+  augroup END
+]])
